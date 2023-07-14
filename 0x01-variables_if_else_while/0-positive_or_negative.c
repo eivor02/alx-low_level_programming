377c278e2b8b04884860e92cc044a93c1175e080
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-14 08:04:59 -0700

    up

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,16 @@ int main(void)
 srand(time(0));
 
 n = rand() - RAND_MAX / 2;
-  if (n > 0) {
-        printf("is positive\n");
+
+
+   if (n > 0) {
+        printf("%d is %s\n", n, "positive");
     } else if (n == 0) {
-        printf("is zero\n");
+        printf(""%d is %s\n", n, "negative");
     } else {
         printf("is negative\n");
     }
+
+    return 0;
+}
+
