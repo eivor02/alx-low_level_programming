06a3e43389ee629a0bde91b33381a8d6b3a462bd
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-14 15:27:18 -0700

    up

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Saif Eddine Belhaj
+ */
+
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int S;
+
+	for (S = 0; S < 10; i++)
+	{
+		printf("%S", S);
+	}
+	putchar('\n');
+
+	return (0);
+
