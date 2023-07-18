5f734e5899e88103fcfb2848440ecaa4235a19f5
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 13:37:01 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+/*
+ * File: 6-abs.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+
+int _abs(int s)
+{
+	if (s < 0)
+	{
+		return (s * -1);
+	}
+	else
+	{
+		return (s);
+	}
+}
+
+
+
