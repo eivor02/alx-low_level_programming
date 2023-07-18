f0bbb37d15b08622ed745b6035da54f5aafccf11
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 09:46:01 -0700

    pk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - print "a to z" in lowercase, followed by a new line. 
+ * Return: always success
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+
+	_putchar('\n');
+}
