32339caf4f08f063ee1ba43aff0a87bacc5c5cef
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 14:30:35 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,49 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ *
+ * Return: times table
+ */
+
+void times_table(void)
+{
+	int s, b;
+
+	s= 0;
+	while (s<= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if ((s* b) > 9)
+			{
+				_putchar(((s* b) / 10) + '0');
+				_putchar(((s* b) % 10) + '0');
+			}
+			else if (b != 0)
+			{
+				_putchar(' ');
+				_putchar((s* b) + '0');
+			}
+			else
+			{
+				_putchar((s* b) + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		_putchar('\n');
+	}
+}
+
