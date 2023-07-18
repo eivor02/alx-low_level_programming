689cea9599ee04a8edf2d4ed6763e5a8a80a30d1
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 10:22:03 -0700

    up

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,7 @@
 #include "main.h"
 
 /**
- * Description: Write program to print alphabet in lowercase using _putchar 
- * followed by a new line.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  * Return: Always 0.
  */
 
