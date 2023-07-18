476b91b0462a9a8d4196ccc6f9453c9f2f6cd41a
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 14:47:47 -0700

    work done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+
+/*
+ * File: 10-add.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @s: int to be added to b
+ * @b: int to be added to s
+ * Return: sum of s and b
+ */
+int add(int s, int b)
+{
+int c;
+c = s + b;
+return (c);
+}
+
