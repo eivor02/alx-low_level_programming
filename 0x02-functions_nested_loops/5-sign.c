ae05fdc6a26761869d3b41ab73a11c1e6f617225
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-18 12:50:36 -0700

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * File: 5-sign.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+
+/**
+ * Return: 1 if the number is > than zero,
+ * 0 if the number is zero,
+ * -1 if the number is < than zero.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
