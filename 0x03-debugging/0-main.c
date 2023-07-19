5740dcc0b4bc87f0f316e86aef9c260d8e77feda
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-19 05:03:20 -0700

    done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-main.c
+ * Auth: Saif Eddine belhaj
+ *
+ */
+
+#include "main.h"
+
+/**
+ *main - Debugging(tests function that prints if integer is positive or negative)
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
