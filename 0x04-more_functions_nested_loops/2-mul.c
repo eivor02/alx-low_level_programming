e53c5ec03a0730a73533e070b954ff43edf68f1f
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-20 13:03:43 -0700

    work done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth:  Saif Eddine belhaj
+ */
+
+#include "main.h"
+
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
