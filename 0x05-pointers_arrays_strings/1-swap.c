affdc26111fb2cfd8f8c9e2f524ac2abdcf84b3f
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-24 09:24:56 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - Swaps the values of two integers.
+* @a: the first int
+* @b: the second int
+* Return 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	aux = *a;
+	*a = *b;
+	*b = s;
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Pointers, arrays and strings
+In this project, I learned about what and how to use pointers and arrays and the differences between the two as well as how to use and manipulate strings in C.
+
+
