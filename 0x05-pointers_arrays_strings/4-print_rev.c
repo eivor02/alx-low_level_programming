a444ce71aad17e04e4476b46fd763ef3cd07a790
Saif Eddine Belhaj <saifeddine.belhaj@gmail.com>
2023-07-24 10:54:20 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse.
+ * @se the string to be printed in rev
+ */
+
+void print_rev(char *s)
+{
+	int e, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (e = n - 1; e >= 0; e--)
+	{
+		_putchar(s[e]);
+	}
+	_putchar('\n');
+}
+
