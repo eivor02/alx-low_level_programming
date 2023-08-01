ae4567ae6dc17e3310b30254cecd427a0c670e39
Saif <136341062+eivor02@users.noreply.github.com>
2023-08-01 03:13:56 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
 /**
- * _strstr - locate a substring
- * @haystack: the string to search
- * @needle: the string to find
- *
- * Return: char value
- */
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-    int a = 0, b = 0;
+	char *bhaystack;
+	char *pneedle;
 
-    while (haystack[a])
-    {
-        while (needle[b])
-        {
-            if (haystack[a + b] != needle[b])
-            {
-                break;
-            }
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
 
-            b++;
-        }
-
-        if (needle[b] == '\0')
-        {
-            return (haystack + a);
-        }
-
-        a++;
-    }
-
-    return ('\0');
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
 }
