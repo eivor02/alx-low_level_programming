e46727b3eb018a7d1397b09002e51d9e0cbb46c6
Eivor02 <saifeddine.belhaj@gmail.com>
2023-08-16 16:29:40 +0100

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,20 @@
 #include "function_pointers.h"
 
 /**
-
-array_iterator - Applies a function to each element of an integer array.
-
-@param array An input integer array.
-@param size The size of the array.
-@param action A pointer to the function to be applied.
-
-@return No return value.
-*/
+ * array_iterator - Applies a function to each element of an integer array.
+ * @param array An input integer array.
+ * @param size The size of the array.
+ * @param action A pointer to the function to be applied.
+ *
+ * @return No return value.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     // Avoid using magic numbers and use a named constant instead
-    static const unsigned int MAX_ITERATIONS = size;
+    static const size_t MAX_ITERATIONS = size;
 
     // Use a for loop instead of a while loop
-    for (unsigned int i = 0; i < MAX_ITERATIONS; i++) {
+    for (size_t i = 0; i < MAX_ITERATIONS; i++) {
         action(array[i]);
     }
 }
