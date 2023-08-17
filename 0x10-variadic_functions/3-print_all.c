5115f6984f6e0fe0cbe4d2ece2b974c4b8fdca4d
Saif <136341062+eivor02@users.noreply.github.com>
2023-08-17 13:07:36 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,61 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include "variadic_functions.h"
 
-void print_all(const char *format, ...) {
-    // Create a va_list to store the arguments
-    va_list valist;
+/**
+ * print_all - Prints any combination of characters, integers, floats, and strings.
+ * @format: A list of types of arguments passed to the function.
+ *
+ * Return: No return.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0;
+	char *str;
+	const char valid_types[] = "cifs";
 
-    // Initialize the va_list with the format string and arguments
-    va_start(valist, format);
+	va_start(valist, format);
 
-    // Iterate over the format string
-    for (size_t i = 0; i < strlen(format); i++) {
-        // Get the current character and its type
-        char c = format[i];
-        int type = _Generic(c, char: 1, int: 2, double: 3, const char *: 4);
+	while (format && format[i])
+	{
+		int found_valid_type = 0;
 
-        // Handle the different types of arguments
-        switch (type) {
-            case 1:
-                // Print a character argument
-                printf("%c", va_arg(valist, int));
-                break;
-            case 2:
-                // Print an integer argument
-                printf("%d", va_arg(valist, int));
-                break;
-            case 3:
-                // Print a double argument
-                printf("%f", va_arg(valist, double));
-                break;
-            case 4:
-                // Print a string argument
-                const char *str = va_arg(valist, const char *);
-                printf("%s", str);
-                break;
-        }
-    }
+		for (unsigned int j = 0; valid_types[j]; j++)
+		{
+			if (format[i] == valid_types[j])
+			{
+				if (i > 0 && format[i - 1])
+					printf(", ");
+				found_valid_type = 1;
+				break;
+			}
+		}
 
-    // Print a newline at the end
-    printf("\n");
+		if (found_valid_type)
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(valist, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double));
+				break;
+			case 's':
+				str = va_arg(valist, char *);
+				if (!str)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+			}
+		}
 
-    // Clean up the va_list
-    va_end(valist);
+		i++;
+	}
+
+	printf("\n");
+	va_end(valist);
 }
